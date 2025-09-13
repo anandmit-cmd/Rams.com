3e39b150b98c925c69a012b68f4654e066b5465a
Anand Kumar Mishra <anandmit9@gmail.com>
2025-09-13 01:37:58 +0000

    Try fixing this error: `Runtime Error: Error: Cannot read properties of

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -277,8 +277,8 @@ export default function LandingPage() {
                     <ul className="space-y-2 text-sm">
                         <li><Link href="#" className="text-gray-400 hover:text-white" prefetch={false}>Blog</Link></li>
                         <li><Link href="#" className="text-gray-400 hover:text-white" prefetch={false}>Help Center</Link></li>
-                        <li><Link href="#" className="text-gray-400 hover:text-white" prefetch={false}>Contact Us</Link></li>
-                        <li><Link href="#" className="text-gray-400 hover:text-white" prefetch={false}>Privacy Policy</Link></li>
+                        <li><Link href="/contact" className="text-gray-400 hover:text-white" prefetch={false}>Contact Us</Link></li>
+                        <li><Link href="/privacy" className="text-gray-400 hover:text-white" prefetch={false}>Privacy Policy</Link></li>
                     </ul>
                 </div>
                 <div>
@@ -308,5 +308,3 @@ export default function LandingPage() {
     </div>
   );
 }
-
-    
