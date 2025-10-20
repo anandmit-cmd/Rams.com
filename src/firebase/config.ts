535af27d126e4a4dcc7163cebd24d7ff8890a8c3
Anand Kumar Mishra <anandmit9@gmail.com>
2025-10-20 04:07:59 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "ramscom-5js0q",
+  "appId": "1:135645483680:web:97a8126b5c3d210842efc5",
+  "apiKey": "AIzaSyCxfePMY6zW7I1elyczkKY_-HqscQFS5gE",
+  "authDomain": "ramscom-5js0q.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "135645483680"
+};
